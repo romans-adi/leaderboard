2e2a243e0910c7b42147dcb4002d028a34bf4547
Romans S <romans.spilaks@gmail.com>
2023-05-16 20:20:29 +0300

    Project setup done: Create setup-project branch under branch development.  Add linters configuration. Add src directory with files index.html, index.js and styles.css. Add README.md. Add webpack configuration. Add postcss and tailwinf configurations for tailwind css. Add .gitignore file. Build public directory as dist

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [['autoprefixer'],
+    'postcss-preset-env',
+    tailwindcss],
+};
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8883,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+  ],
+};
